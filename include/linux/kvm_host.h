f80129c2001603b609095cec4e225e8a6f5b5b27
Sean Christopherson <seanjc@google.com>
2022-08-08 17:21:57 -0700

    KVM: Export symbols for vendor modules iff there will be vendor modules
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Venkatesh Srinivas <venkateshs@chromium.org>
    Signed-off-by: Anish Ghulati <aghulati@google.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -47,6 +47,12 @@
 
 #ifdef __KVM__
 
+#ifdef CONFIG_HAVE_KVM_VENDOR_MODULES
+#define KVM_EXPORT_SYMBOL_GPL(symbol) EXPORT_SYMBOL_GPL(symbol)
+#else
+#define KVM_EXPORT_SYMBOL_GPL(symbol)
+#endif
+
 #ifndef KVM_MAX_VCPU_IDS
 #define KVM_MAX_VCPU_IDS KVM_MAX_VCPUS
 #endif
