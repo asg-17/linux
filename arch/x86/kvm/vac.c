aa09ca3ea85f2bdfac45ece66e882221ab86b3f6
Anish Ghulati <aghulati@google.com>
2023-05-04 20:11:55 +0000

    KVM: x86: Create stubs for a new VAC module
    Signed-off-by: Anish Ghulati <aghulati@google.com>

---
+++ arch/x86/kvm/vac.c
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include "vac.h"
+
+#ifdef CONFIG_HAVE_KVM_VAC
+MODULE_LICENSE("GPL");
+
+static int __init vac_init(void)
+{
+	return 0;
+}
+module_init(vac_init);
+
+static void __exit vac_exit(void)
+{
+}
+module_exit(vac_exit);
+
+#endif // CONFIG_HAVE_KVM_VAC
