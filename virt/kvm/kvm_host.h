1a749ae0c0c6f1e5e7ae3589c5d058fe76d4e75c
Anish Ghulati <aghulati@google.com>
2023-08-08 20:17:37 +0000

    KVM: Copy kvm_dirty_ring.h to virt/kvm
    Signed-off-by: Anish Ghulati <aghulati@google.com>

--- virt/kvm/kvm_host.h
+++ virt/kvm/kvm_host.h
@@ -43,7 +43,7 @@
 #include "kvm_types.h"
 
 #include <asm/kvm_host.h>
-#include <linux/kvm_dirty_ring.h>
+#include "kvm_dirty_ring.h"
 
 #ifndef KVM_MAX_VCPU_IDS
 #define KVM_MAX_VCPU_IDS KVM_MAX_VCPUS
