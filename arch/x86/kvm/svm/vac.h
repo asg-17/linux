397791369c21b0f0e97dc5d52e550ba8126b3743
Anish Ghulati <aghulati@google.com>
2023-10-19 20:31:55 +0000

    KVM: SVM: Move shared SVM data structures into VAC
    svm_cpu_data[] contains per-pCPU state associated with active vCPUs.
    This is data that should be shared across multiple KVM modules and
    therefore belongs in VAC.
    
    Signed-off-by: Venkatesh Srinivas <venkateshs@chromium.org>

---
+++ arch/x86/kvm/svm/vac.h
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0-only
+//
+#ifndef ARCH_X86_KVM_SVM_VAC_H
+#define ARCH_X86_KVM_SVM_VAC_H
+
+#include "../vac.h"
+
+struct svm_cpu_data {
+	u64 asid_generation;
+	u32 max_asid;
+	u32 next_asid;
+	u32 min_asid;
+
+	struct page *save_area;
+	unsigned long save_area_pa;
+
+	struct vmcb *current_vmcb;
+
+	/* index = sev_asid, value = vmcb pointer */
+	struct vmcb **sev_vmcbs;
+};
+
+#endif // ARCH_X86_KVM_SVM_VAC_H
