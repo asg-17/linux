fbf760b94347574e1dd906bb14462c2259076884
Anish Ghulati <aghulati@google.com>
2023-10-19 20:35:10 +0000

    KVM: VMX: Move shared VMX data structures into VAC
    vmxarea and current_vmcs will be shared across multiple KVM modules so
    they should be moved into VAC.
    Move VPID bitmap into the VAC
    
    Signed-off-by: Venkatesh Srinivas <venkateshs@chromium.org>

---
+++ arch/x86/kvm/vmx/vac.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <asm/vmx.h>
+
+#include "../vac.h"
+#include "vmcs.h"
+
+void vac_set_vmxarea(struct vmcs *vmcs, int cpu);
+
+struct vmcs *vac_get_vmxarea(int cpu);
+int allocate_vpid(void);
+void free_vpid(int vpid);
